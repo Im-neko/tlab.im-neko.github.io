99ba0ce9e52157388a0af75f65757980d06d1645
im-neko <lukepuyuki0430@gmail.com>
2018-07-29 03:36:03 +0900

    wip

--- api/app.js
+++ api/app.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 app.use(function(req, res, next) { // {{{
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "portal.keioac.jp, localhost");
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", " x-access-token, Origin, X-Requested-With, Content-Type, Accept");
   if(req.method == "OPTIONS"){
